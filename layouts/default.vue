d8cbc7f10112ebcce8c6aed063189539713ab4fc
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-05-05 20:09:03 -0300

    Add NuxtImg, carousel and mock images

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="w-full flex justify-center border border-green-500">
+  <div class="w-full flex justify-center items-center h-12 my-auto">
     <h1>
       {{ headerNavBarText }}
     </h1>
   </div>
   <slot />
 
-  <div>
+  <div class="flex justify-center mt-6">
     <p>{{ footerRightsText }}</p>
   </div>
 </template>
