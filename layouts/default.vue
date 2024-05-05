7ebeed6967da5287d0d128381918d96e948f0550
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-05-05 19:15:40 -0300

    Add layout, css asset

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full flex justify-center border border-green-500">
+    <h1>
+      {{ headerNavBarText }}
+    </h1>
+  </div>
+  <slot />
+
+  <div>
+    <p>{{ footerRightsText }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const headerNavBarText: string = 'Bedotte'
+
+
+const footerRightsText: string = 'Bedotte 2024 © All rights reserved.'
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-red-500">
-    oi
+  <div>
+    oioioioi
   </div>
 </template>
 
