8e7cec50f070b46722c88d7b8fb491842c5e2058
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-05-07 16:57:51 -0300

    Refactor text

--- pages/shaper/index.vue
+++ pages/shaper/index.vue
@@ -22,12 +22,22 @@
         {{ thirdParagraph }}
       </p>
 
+      <UButton
+        variant="soft"
+        class="text-xl mt-12 tracking-wider"
+        @click="navigateToBoardsPage"
+      >
+        Ver catálogo >
+      </UButton>
+
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
-const firstParagraph: string = 'A Cedotte Shapes começa quando o shaper Paulo Cedotte decide viajar pelo mundo atrás de ondas e com um sonho na cabeça: parar na Austrália e aprender as melhores técnicas de shape e fabricação de pranchas em um dos locais mais tradicionais do surf mundial: Gold Coast, na praia de Snapper Rocks.'
-const secondParagraph: string = 'Surpreendido pelas oportunidades que apareceram, teve acesso às melhores fábricas de pranchas e aos ensinamentos que nem mesmo os próprios australianos tinham, pois os mestres não o viam como um futuro concorrente.'
-const thirdParagraph: string = 'Inspirado por tudo o que aprendeu e com a confiança de quem adquiriu respeito dos melhores shapers do mundo, começa a desenvolver seus modelos de pranchas. Todas elas foram criadas com a supervisão de mestres do shape australiano.'
+import { firstParagraph, secondParagraph, thirdParagraph } from '@/utils/shaper/paragraphs'
+
+const navigateToBoardsPage = async () => {
+  await navigateTo('/boards')
+}
 </script>
---
+++ utils/shaper/paragraphs.ts
@@ -0,0 +1,9 @@
+const firstParagraph: string = 'A Cedotte Shapes começa quando o shaper Paulo Cedotte decide viajar pelo mundo atrás de ondas e com um sonho na cabeça: parar na Austrália e aprender as melhores técnicas de shape e fabricação de pranchas em um dos locais mais tradicionais do surf mundial: Gold Coast, na praia de Snapper Rocks.'
+const secondParagraph: string = 'Surpreendido pelas oportunidades que apareceram, teve acesso às melhores fábricas de pranchas e aos ensinamentos que nem mesmo os próprios australianos tinham, pois os mestres não o viam como um futuro concorrente.'
+const thirdParagraph: string = 'Inspirado por tudo o que aprendeu e com a confiança de quem adquiriu respeito dos melhores shapers do mundo, começa a desenvolver seus modelos de pranchas. Todas elas foram criadas com a supervisão de mestres do shape australiano.'
+
+export {
+  firstParagraph,
+  secondParagraph,
+  thirdParagraph
+}
